391728764758d3f690f3d0675501f8ad1008d7a8
Yahyr Paredes <yahyrparedesarteaga@gmail.com>
2022-01-13 11:27:02 -0500

    fix RxFormBuilder

--- ceabe-front-auth/src/app/app.module.ts
+++ ceabe-front-auth/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {FormsModule} from '@angular/forms';
 import {MenuComponent} from './menu/menu.component';
 import {SignupComponent} from './signup/signup.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {RxFormBuilder} from "@rxweb/reactive-form-validators";
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import {AppRoutingModule} from "./app-routing.module";
     HttpClientModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [RxFormBuilder],
   bootstrap: [AppComponent]
 })
 export class AppModule {
